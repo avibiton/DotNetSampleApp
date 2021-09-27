7cc21b6060eb2679389defffebaa03a91f841075
avibiton <31897047+avibiton@users.noreply.github.com>
2021-09-27 17:28:02 +0000

    Update test-nunit.bat

--- test-nunit.bat
+++ test-nunit.bat
@@ -1,14 +1,14 @@
 @echo off
-set SL_TOOLS_PATH=..\SL.DotNet-2.0.1.741\x86\
+set SL_TOOLS_PATH=C:\Utils\SeaLights\x64\
 set SL_AGENT=%SL_TOOLS_PATH%sl.dotnet.exe
 
 set SL_ENV_NAME="Unit Tests-NUnit"
 echo Running tests
 del TestResult.xml
 @rem %SL_AGENT% startBackgroundTestListener
-%SL_AGENT% startExecution --TokenFile sealights.token --buildSessionIdFile buildSession --testStage %SL_ENV_NAME% --proxy http://127.0.0.1:8888
-%SL_AGENT% testListener --logAppendFile true --tokenfile sealights.token --buildSessionIdFile buildSession --logAppendFile true --logAppendConsole true --target "packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" --targetArgs "MyCalculatorNUnitTests\bin\Debug\MyCalculatorNUnitTests.dll" --proxy http://127.0.0.1:8888
-%SL_AGENT% endExecution --TokenFile sealights.token --buildSessionIdFile buildSession --testStage %SL_ENV_NAME% --proxy http://127.0.0.1:8888
+%SL_AGENT% startExecution --TokenFile sltoken.txt --buildSessionIdFile buildSessionId.txt --testStage %SL_ENV_NAME%
+%SL_AGENT% testListener --logAppendFile true --tokenfile sltoken.txt --buildSessionIdFile buildSessionId.txt --logAppendFile true --logAppendConsole true --target "packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" --targetArgs "MyCalculatorNUnitTests\bin\Debug\MyCalculatorNUnitTests.dll"
+%SL_AGENT% endExecution --TokenFile sltoken.txt --buildSessionIdFile buildSessionId.txt --testStage %SL_ENV_NAME%
 echo Uploading report
-%SL_AGENT% reportParser --tokenfile sealights.token --buildSessionIdFile buildSession --report TestResult.xml --testStage %SL_ENV_NAME% --proxy http://127.0.0.1:8888
+%SL_AGENT% reportParser --tokenfile sltoken.txt --buildSessionIdFile buildSessionId.txt --report TestResult.xml --testStage %SL_ENV_NAME%
 
