72472671f0fe784b92ec796ef42b505c8ce563d1
avibiton <31897047+avibiton@users.noreply.github.com>
2021-09-27 17:33:14 +0000

    Update build-and-test.bat

--- build-and-test.bat
+++ build-and-test.bat
@@ -1,6 +1,6 @@
 @echo off
 set SL_PROXY=http://127.0.0.1:8888
-set SL_TOOLS_PATH=..\SL.DotNet-2.0.1.741\x64\
+set SL_TOOLS_PATH=C:\Utils\SeaLights\x64\
 call build.bat
 call test.bat
 call test-nunit.bat
